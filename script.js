3383427e7bb25ebceef7c668375c4e648b2db116
alfredoy98 <alfredoyax98@yahoo.com>
2016-03-14 10:38:28 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,8 +1,29 @@
-$('#clock').fitText(1.3);
+$(document).ready(function(){
+   setInterval('updateClock()', 1000);
+});
 
-function update() {
-  $('#clock').html(moment().format('D. MMMM YYYY H:mm:ss'));
-}
+function updateClock (){
+ 	var currentTime = new Date ( );
+  	var currentHours = currentTime.getHours ( );
+  	var currentMinutes = currentTime.getMinutes ( );
+  	var currentSeconds = currentTime.getSeconds ( );
 
-update();
-setInterval(update, 1000);
+  	// Pad the minutes and seconds with leading zeros, if required
+  	currentMinutes = ( currentMinutes < 10 ? "0" : "" ) + currentMinutes;
+  	currentSeconds = ( currentSeconds < 10 ? "0" : "" ) + currentSeconds;
+
+  	// Choose either "AM" or "PM" as appropriate
+  	var timeOfDay = ( currentHours < 12 ) ? "AM" : "PM";
+
+  	// Convert the hours component to 12-hour format if needed
+  	currentHours = ( currentHours > 12 ) ? currentHours - 12 : currentHours;
+
+  	// Convert an hours component of "0" to "12"
+  	currentHours = ( currentHours == 0 ) ? 12 : currentHours;
+
+  	// Compose the string for display
+  	var currentTimeString = currentHours + ":" + currentMinutes + ":" + currentSeconds + " " + timeOfDay;
+  	
+  	
+   	$("#clock").html(currentTimeString);	  	
+ }
