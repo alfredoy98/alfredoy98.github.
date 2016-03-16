8172f68498193d1503b4448d8ae7881dff805c7d
alfredoy98 <alfredoyax98@yahoo.com>
2016-03-16 08:39:38 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,30 +1,30 @@
 $(document).ready(function(){
-   setInterval('updateClock()', 1000);
+   // setInterval(updateClock(), 1000);
+   setInterval(function () { $('#clock').text(Date()) }, 1000)
 });
 
-function updateClock (){
- 	var currentTime = new Date ( );
-  	var currentHours = currentTime.getHours ( );
-  	var currentMinutes = currentTime.getMinutes ( );
-  	var currentSeconds = currentTime.getSeconds ( );
+// function updateClock (){
+//  	var currentTime = new Date ( );
+//  	var currentHours = currentTime.getHours();
+//  	var currentMinutes = currentTime.getMinutes();
+//  	var currentSeconds = currentTime.getSeconds();
 
-  	// Pad the minutes and seconds with leading zeros, if required
-  	currentMinutes = ( currentMinutes < 10 ? "0" : "" ) + currentMinutes;
-  	currentSeconds = ( currentSeconds < 10 ? "0" : "" ) + currentSeconds;
+//  	// Pad the minutes and seconds with leading zeros, if required
+//  	currentMinutes = ( currentMinutes < 10 ? "0" : "" ) + currentMinutes;
+//  	currentSeconds = ( currentSeconds < 10 ? "0" : "" ) + currentSeconds;
 
-  	// Choose either "AM" or "PM" as appropriate
-  	var timeOfDay = ( currentHours < 12 ) ? "AM" : "PM";
+//  	// Choose either "AM" or "PM" as appropriate
+//  	var timeOfDay = ( currentHours < 12 ) ? "AM" : "PM";
 
-  	// Convert the hours component to 12-hour format if needed
-  	currentHours = ( currentHours > 12 ) ? currentHours - 12 : currentHours;
+//  	// Convert the hours component to 12-hour format if needed
+//  	currentHours = ( currentHours > 12 ) ? currentHours - 12 : currentHours;
 
-  	// Convert an hours component of "0" to "12"
-  	currentHours = ( currentHours == 0 ) ? 12 : currentHours;
+//  	// Convert an hours component of "0" to "12"
+//  	currentHours = ( currentHours == 0 ) ? 12 : currentHours;
 
-  	// Compose the string for display
-  	var currentTimeString = currentHours + ":" + currentMinutes + ":" + currentSeconds + " " + timeOfDay;
+//  	// Compose the string for display
+//  	var currentTimeString = currentHours + ":" + currentMinutes + ":" + currentSeconds + " " + timeOfDay;
   	
   	
-   	$("#clock").html(currentTimeString);	  	
- }
-<script src = "script.js"></script> 
+//    	$("#clock").html(currentTimeString);
+// }
